e63e20c9f0e791dcfde72ff88f648b278beaa26a
sshhaahh <ameenfalak786@gmail.com>
2024-12-09 16:36:16 +0530

    test

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
   return (
     <div className='flex flex-col min-h-screen '>
       <Header/>
-      <div className=' flex-grow flex justify-center items-center mb-4'>
+      <div className=' flex-grow flex justify-center items-center mb-8'>
         <Blogs />
       </div>
       <div className='fixed bottom-0 w-full bg-white z-10'>
